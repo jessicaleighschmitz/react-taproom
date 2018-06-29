e89604b0d915f6386e3eb9031b3ea277cd421841
Jessica Schmitz <schmitz.jessical@gmail.com>
2018-06-29 10:31:00 -0700

    add header content with links

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import logo from '../assets/img/logo.png';
 
 function Header () {
   return(
     <div>
-      <p><Link to='/newsTicker'>Craft Brew News</Link></p>
+      <div className="header-flex">
+      <p className='home'><Link to='/'>Home</Link></p>
+      <p className='newsLink'><Link to='/newsTicker'>Craft Brew News</Link></p>
+      </div>
+      <div className='logo'>
+        <p><Link to='/'><img src={logo}/></Link></p>
+        <h1><Link to='/'>Fermented</Link></h1>
+      </div>
     </div>
   );
 }
