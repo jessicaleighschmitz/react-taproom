80fdb9fb05c603ccfed5ecdf8e5b5040afaad62f
Jessica Schmitz <schmitz.jessical@gmail.com>
2018-06-29 16:11:22 -0700

    add footer content and styles

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,15 +1,54 @@
 import React from 'react';
+import instagram from '../assets/img/instagram.png';
+import facebook from '../assets/img/facebook.png';
+import twitter from '../assets/img/twitter.png';
 
 function Footer () {
   return(
     <div className='footer'>
-      <p>k</p>
+      <div className="social">
+        <img src={instagram}/>
+        <img src={facebook}/>
+        <img src={twitter}/>
+      </div>
+      <div>
+        <div className="footer-container">
+          <p>&copy;2018 Fermented, LLC. </p>
+          <div className="contact">
+            <p>Contact</p>
+            <p>About</p>
+          </div>
+        </div>
+      </div>
       <style jsx>{`
         *{
           margin: 0;
         }
         .footer{
           background-color: #E48F3B;
+          min-height: 100px;
+        }
+        p{
+          color: #000;
+        }
+        .social{
+          display: flex;
+          justify-content: center;
+          padding-top: 30px;
+        }
+        img{
+          padding: 15px;
+        }
+        .contact{
+          display:flex;
+          justify-content: flex-end;
+        }
+        .footer-container{
+          display: flex;
+          justify-content: space-between;
+        }
+        .contact p, .footer-container p{
+          padding: 15px;
         }
             `}</style>
     </div>
