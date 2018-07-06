034182a4e60da543ad8dc65b9e0f7c839dab0b57
Jessica Schmitz <schmitz.jessical@gmail.com>
2018-07-06 11:34:55 -0700

    add button styles

--- src/components/Admin.jsx
+++ src/components/Admin.jsx
@@ -9,7 +9,17 @@ function Admin (props){
       <BeerList
         beerList={props.beerList}
         currentRouterPath={props.currentRouterPath} />
-      <button type="button"><Link to="/AddBeerForm">Add Beer</Link></button>
+      <button type="button"><Link style={{textDecoration: "none", color: "#E48F3B"}} to="/AddBeerForm">Add Beer</Link></button>
+      <style jsx>{`
+          button{
+            width: 150px;
+            height: 40px;
+            border: 2px solid #E48F3B;
+            font-size: 1.2rem;
+            border-radius: 6px;
+            margin: 0 auto 30px;
+          }
+            `}</style>
     </div>
   );
 }
