17a770be7d4754caf38fe952cb8ed728fb1e8158
Jessica Schmitz <schmitz.jessical@gmail.com>
2018-07-06 10:45:16 -0700

    add a link in admin for the add beer form

--- src/components/Admin.jsx
+++ src/components/Admin.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import BeerList from './BeerList';
+import { Link } from 'react-router-dom';
 
 function Admin (props){
   return(
     <div>
       <div>yoooo</div>
+      <button type="button"><Link to="/AddBeerForm">Add Beer</Link></button>
       <BeerList
         beerList={props.beerList}
         currentRouterPath={props.currentRouterPath} />
